8a0677e1068e61701aeeab2236f1dd8b91258478
Madalambana <almadalambana@gmail.com>
2023-09-10 20:59:44 +0200

    set-up

---
+++ src/Pages/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import '../Stylesheets/index.css'
+
+const Index = () => {
+
+    return (
+      <div className="navBar">
+        <h2><a href="#">eXCLSUIVE</a></h2>
+     <nav >
+        <ul>
+            <li><a  href="about.html">About</a></li>
+            <li><a  href="contact.html">Contact</a></li>
+            <li><a  href="workExp.html">Education</a></li>
+            <li><a  href="testimonials.html">Testimonials</a></li>
+        </ul>
+    </nav>
+      </div>
+    );
+  }
+  
+  export default Index;
