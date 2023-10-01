b1d5e990d7b23bfb6ec735786028cb006cd64bf3
Madalambana <almadalambana@gmail.com>
2023-10-01 10:57:16 +0200

    copyright-update

--- src/components/copy.js
+++ src/components/copy.js
@@ -1,12 +1,28 @@
 import React from 'react';
-import '../Stylesheets/copy.css'
+import '../Stylesheets/copy.css';
+import Clear from './clear';
 
 const Copyright = () => {
 
     return (
       <div className='copyright'>
         <footer>
-    <p>Copyright &copy; 2023</p>
+          <div className='cpy'>
+          <p>Copyright &copy; 2023</p>
+          </div>
+         
+          <div className='box1'>
+          <ul><a href=''>Privacy Policy</a></ul>
+          <ul><a href=''>Disclaimer</a></ul>
+          </div>
+        
+          <div className='box2'>
+          <ul><a href=''>Terms and conditions</a></ul>
+          <ul><a href=''>About Us</a></ul>
+          </div>
+          
+
+    
     </footer>
       </div>
     );
