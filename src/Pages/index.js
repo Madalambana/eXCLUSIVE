98da1c8ab4219b25827ae80b046d45713dbc90af
Madalambana <almadalambana@gmail.com>
2023-10-01 14:56:44 +0200

    index-update

--- src/Pages/index.js
+++ src/Pages/index.js
@@ -2,15 +2,48 @@ import React from 'react';
 import Copyright from '../components/copy';
 import Navbar from '../components/navbar';
 import '../Stylesheets/indx.css';
-// import Clear from './clear';
+import Clear from '../components/clear';
 
 const Index = () => {
 
     return (
       <div>
         <div className='banner'><strong>eXCLUSIVE</strong></div>
-
         <Navbar/>
+        <body>
+
+          {/* info box1 */}
+          <div className='info-box1'>
+            <div className='img1'></div>
+            <div className='sec1'>
+             <div className='header1'>
+                <p>mzansi tings </p>
+              </div>
+              <div className='info'>
+              <p>Quis velit non incididunt aliqua laborum cupidatat ex reprehenderit. Occaecat ad adipisicing nostrud ea nulla occaecat ut. Irure aliqua proident nisi laborum elit sit tempor ad officia consectetur est proident pariatur veniam
+              Quis velit non incididunt aliqua laborum cupidatat ex reprehenderit. Occaecat ad adipisicing nostrud ea nulla occaecat ut. Irure aliqua proident nisi laborum elit sit tempor ad officia consectetur est proident pariatur veniam
+                .</p>
+              </div>
+            
+            <div>              
+              </div>
+            </div>
+          </div>
+   
+
+          {/* info box2 */}
+          <div className='info-box1'>
+            <div className='#'></div>
+
+          </div>
+
+          {/* info box3 */}
+          <div className='info-box1'>
+            <div className='#'></div>
+
+          </div>
+
+        </body>
         <Copyright/>
       </div>
     );
---
+++ src/components/clear.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const clearStyle = {
+  clear: 'both'
+};
+
+const Clear = () => {
+  return (
+    <div className="clear" style={clearStyle}>
+    </div>
+  );
+};
+
+export default Clear;
