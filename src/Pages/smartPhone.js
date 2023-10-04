da3058d37f88ed7b28c602615c2640d7f39fbffb
Madalambana <almadalambana@gmail.com>
2023-10-04 07:45:53 +0200

    body-I

--- src/Pages/smartPhone.js
+++ src/Pages/smartPhone.js
@@ -17,83 +17,96 @@ const Smartphones = (props) => {
         <div className="header">
           <h3>realme</h3>
         </div>
+        <Clear/>
 
         {/* quadrant 1 */}
         <div className="floatlft">
-          <div className="rm1"></div>
-          <p className="rmi">
-            {" "}
-            Lorem ipsum dolor sit amet,consectetur adipiscing elit, sed
-            doeiusmod tempor ut labore et dolore magna . Ut enim adminim veniam,
-            quis nostrudexercitation ullamco laboris nisi ut aliquip ex ea
-            commodoconsequat.{" "}
-          </p>
+        <div className="img1"></div>
+        <div className="mini-inf">
+            <p>
+              {" "}
+              Lorem ipsum dolor sit amet,consectetur adipiscing elit, sed
+              doeiusmod tempor ut labore et dolore magna . Ut enim adminim
+              veniam, quis nostrudexercitation ullamco laboris nisi ut aliquip
+              ex ea commodoconsequat.{" "}
+            </p>
+          </div>
 
-          <p className="purchaseBtn">
-            <a href="selection" className="a3">
-              Purchase{" "}
+          <button>
+            <a href="#" className="a">
+              Purchase
             </a>
-          </p>
+          </button>
         </div>
 
         {/* quadrant 2 */}
         <div className="floatrght">
-          <div className="rm1"></div>
-          <p className="rmi">
-            {" "}
-            Lorem ipsum dolor sit amet,consectetur adipiscing elit, sed
-            doeiusmod tempor ut labore et dolore magna . Ut enim adminim veniam,
-            quis nostrudexercitation ullamco laboris nisi ut aliquip ex ea
-            commodoconsequat.{" "}
-          </p>
+          <div className="img1"></div>
+          <div className="mini-inf">
+            <p>
+              {" "}
+              Lorem ipsum dolor sit amet,consectetur adipiscing elit, sed
+              doeiusmod tempor ut labore et dolore magna . Ut enim adminim
+              veniam, quis nostrudexercitation ullamco laboris nisi ut aliquip
+              ex ea commodoconsequat.{" "}
+            </p>
+          </div>
 
-          <p className="purchaseBtn">
-            <a href="selection" className="a3">
-              Purchase{" "}
+          <button>
+            <a href="#" className="a">
+              Purchase
             </a>
-          </p>
+          </button>
         </div>
 
         <Clear />
 
         {/* quadrant 3 */}
         <div className="floatlft">
-          <div className="rm1"></div>
-          <p className="rmi">
-            {" "}
-            Lorem ipsum dolor sit amet,consectetur adipiscing elit, sed
-            doeiusmod tempor ut labore et dolore magna . Ut enim adminim veniam,
-            quis nostrudexercitation ullamco laboris nisi ut aliquip ex ea
-            commodoconsequat.{" "}
-          </p>
+        <div className="img1"></div>
+        <div className="mini-inf">
+            <p>
+              {" "}
+              Lorem ipsum dolor sit amet,consectetur adipiscing elit, sed
+              doeiusmod tempor ut labore et dolore magna . Ut enim adminim
+              veniam, quis nostrudexercitation ullamco laboris nisi ut aliquip
+              ex ea commodoconsequat.{" "}
+            </p>
+          </div>
 
-          <p className="purchaseBtn">
-            <a href="selection" className="a3">
-              Purchase{" "}
+          <button>
+            <a href="#" className="a">
+              Purchase
             </a>
-          </p>
+          </button>
         </div>
 
         {/* quadrant 4 */}
         <div className="floatrght">
-          <div className="rm1"></div>
-          <p className="rmi">
-            {" "}
-            Lorem ipsum dolor sit amet,consectetur adipiscing elit, sed
-            doeiusmod tempor ut labore et dolore magna . Ut enim adminim veniam,
-            quis nostrudexercitation ullamco laboris nisi ut aliquip ex ea
-            commodoconsequat.{" "}
-          </p>
+        <div className="img1"></div>
+        <div className="mini-inf">
+            <p>
+              {" "}
+              Lorem ipsum dolor sit amet,consectetur adipiscing elit, sed
+              doeiusmod tempor ut labore et dolore magna . Ut enim adminim
+              veniam, quis nostrudexercitation ullamco laboris nisi ut aliquip
+              ex ea commodoconsequat.{" "}
+            </p>
+          </div>
 
-          <p className="purchaseBtn">
-            <a href="selection" className="a3">
-              Purchase{" "}
+          <button>
+            <a href="#" className="a">
+              Purchase
             </a>
-          </p>
+          </button>
         </div>
 
         <Clear />
-        <div className="more"><a href="#"></a></div>
+        
+          <a href="#">
+          <div className="more"></div>
+          </a>
+        
       </div>
       <Copyright />
     </div>
